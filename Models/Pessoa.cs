561338601dc9fa4449f8344f12e2d2d096099faa
JefersonRodrigues21 <dev.geekjs@gmail.com>
2025-08-05 17:21:22 -0300

    Validando Get e Set

---
+++ Models/Pessoa.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExemploExplorando.Models
+{
+    public class Pessoa
+    {
+        private string _nome;
+        private int _idade;
+
+        public string Nome
+        {
+            // Propriedade Nome com validação e formatação
+            // Retorna o nome em letras maiúsculas
+            // Lança uma exceção se o nome for vazio
+            // pode ser reduzido como: get => _nome.ToUpper(); 
+            get
+            {
+                return _nome.ToUpper();
+            }
+
+            set
+            {
+                if (value == "")
+                {
+                    throw new ArgumentException("O nome não pode ser vazio!");
+                }
+                _nome = value;
+            }
+        }
+
+
+
+        public int Idade
+        {
+            get => _idade;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("A idade não pode ser negativa!");
+                }
+                _idade = value;
+            }
+        }
+        public void Apresentar()
+        {
+            Console.WriteLine($"Nome: {Nome}, Idade: {Idade}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿using ExemploExplorando.Models;
+
+Pessoa p1 = new Pessoa();
+p1.Nome = "Jeferson Rodrigues";
+p1.Idade = -8;
+p1.Apresentar();
