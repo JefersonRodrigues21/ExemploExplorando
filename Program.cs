9111fb445cea3795540e50cfb22151942a43c5b5
JefersonRodrigues21 <dev.geekjs@gmail.com>
2025-08-07 17:45:09 -0300

    Implementando o método listar alunos

--- Program.cs
+++ Program.cs
@@ -3,5 +3,23 @@
 Pessoa p1 = new Pessoa();
 p1.Nome = "Jeferson";
 p1.Sobrenome = "Rodrigues";
-p1.Idade = 38;
-p1.Apresentar();
+
+Pessoa p2 = new Pessoa();
+p2.Nome = "Miguel";
+p2.Sobrenome = "Rodrigues";
+
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Inglês";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.ListarAlunos();
+
+//----------------------------------------------------
+// Pessoa p1 = new Pessoa();
+// p1.Nome = "Jeferson";
+// p1.Sobrenome = "Rodrigues";
+// p1.Idade = 38;
+// p1.Apresentar();
